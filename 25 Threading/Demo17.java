a13b85217ced9c1cf01c95b1b1bd140dee4daf9d
gauravKothari <kotharigaurav6@gmail.com>
2025-07-02 17:06:28 +0530

    package

---
+++ 25 Threading/Demo17.java
@@ -0,0 +1,43 @@
+// program showing the concept of deadlock
+
+class Pen{}
+class Paper{}
+class Demo17{
+    public static void main(String args[]){
+        Pen pn = new Pen();
+        Paper pr = new Paper();
+
+        new Thread(){
+            @Override
+            public void run(){
+                synchronized(pn){
+                    System.out.println("Thread 1 uses Pen and needs Paper");
+                }
+                try{
+                    Thread.sleep(1000);
+                }catch(InterruptedException e){
+                    System.out.println("Exception : "+e);
+                }
+                synchronized(pr){
+                    System.out.println("Thread 1 uses Paper and needs Pen");
+                }
+            }
+        }.start();
+        new Thread(){
+            @Override
+            public void run(){
+                synchronized(pr){
+                    System.out.println("Thread 2 uses Paper and needs Pen");
+                }
+                try{
+                    Thread.sleep(1000);
+                }catch(InterruptedException e){
+                    System.out.println("Exception : "+e);
+                }
+                synchronized(pn){
+                    System.out.println("Thread 2 uses Pen and needs Paper");
+                }
+            }
+        }.start();
+    }
+}
---
+++ 26 Package/Demo1.java
@@ -0,0 +1,8 @@
+// example showing the concept of package
+
+package myPackage;
+class Demo1{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
